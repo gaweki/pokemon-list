ae85c4bf3e50a53760c6478391c505848eb664c6
andreltokdis <andrel@tokodistributor.com>
2020-10-20 14:14:41 +0700

    refactor: Delete unsed comment

--- src/store/detail/detail.js
+++ src/store/detail/detail.js
@@ -4,10 +4,6 @@ const DEFAULT_DATA = {
     detail: {}
 };
 
-// create a store
-// subscribe    -> must be exported, will discuss it in future article
-// set          -> allows you to set a value to store
-// update       -> receives a current store value as input and returns a new one.
 const { subscribe, set, update } = writable(DEFAULT_DATA);
 
 const setCurrentDetailPoke = newDetail => update(detail => {
--- src/store/detail/my-pokemon.js
+++ src/store/detail/my-pokemon.js
@@ -2,10 +2,6 @@ import { writable } from 'svelte/store';
 
 const DEFAULT_DATA = [];
 
-// create a store
-// subscribe    -> must be exported, will discuss it in future article
-// set          -> allows you to set a value to store
-// update       -> receives a current store value as input and returns a new one.
 const { subscribe, set, update } = writable(localStorage.getItem("pokemon_my_list") ? JSON.parse(localStorage.getItem("pokemon_my_list")) : DEFAULT_DATA);
 
 const addPoke = newPoke => update(pokemon => {
