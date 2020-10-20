3fdd425d7d7879de1af77c60949b7b99a76aec20
andreltokdis <andrel@tokodistributor.com>
2020-10-20 15:05:55 +0700

    feat: Add image poke on detail page

--- src/pages/detail/detail.svelte
+++ src/pages/detail/detail.svelte
@@ -7,6 +7,7 @@
   export let name = capitalizeFirstLetter(name)
   let moves = [] 
   let types = []
+  let imgSrc = ""
   name = capitalizeFirstLetter(name)
   onMount(async () => {
     fetch(`${__apps__.env.basePokeAPI}pokemon/${name.toLowerCase()}`)
@@ -14,6 +15,7 @@
       .then(data => {
         moves = data.moves
         types = data.types
+        imgSrc= data.sprites.front_default
 
         let addCurrentDetail = {
           name,
@@ -33,9 +35,20 @@
   ul{
     padding-bottom: 22px;
   }
+  .image-container{
+    text-align: center;
+    height: 99px;
+    width: 100%;
+  }
 </style>
 
 <h1>Detail Page of {name} </h1>
+
+<div class="image-container">
+  {#if imgSrc}
+    <img src={imgSrc} alt={`Image of ${name}`}/>
+  {/if}
+</div>
 
 <h3>Types</h3>
 <ul>
