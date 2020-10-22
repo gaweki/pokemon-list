38abcc1c6f9c396baac8ff538262d4a55eacfb02
andreltokdis <andrel.karunia.s@mail.ugm.ac.id>
2020-10-22 16:54:44 +0700

    chore: Add testing env

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+  transform: {
+    "^.+\\.svelte$": "jest-transform-svelte",
+    "^.+\\.js$": "babel-jest"
+  },
+  "roots": [
+    "<rootDir>"
+  ],
+  "moduleNameMapper": {
+    "@/(.*)$": "<rootDir>/src/$1"
+  },
+  moduleFileExtensions: ["js", "svelte"],
+  testPathIgnorePatterns: ["node_modules"],
+  bail: false,
+  verbose: true,
+  transformIgnorePatterns: ["node_modules/(?!svelte-routing)(?!alias)"],
+  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  "globals": {
+    "fetch": fetch
+  }
+};
