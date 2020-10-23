4d887a030a3b557f0ad7c90afe548115503f6b34
andreltokdis <andrel.karunia.s@mail.ugm.ac.id>
2020-10-23 15:40:21 +0700

    refactor: Refactor handlefetch and random fn

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,8 +2,14 @@ export const capitalizeFirstLetter = (string) => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export const handleFetch = url => {
-  return fetch(url)
-    .then(res => res.json())
-    .then( data => data)
+export const handleFetch = async url => {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(response.statusText);
+  const data = await response.json();
+  return data;
+}
+
+export const getFiftyPercentPossiblity = () => {
+  let mathV = Math.random()
+  return mathV >= 0.5
 }
